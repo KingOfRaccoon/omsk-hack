ac5d1e1af6a65d0f69d70f113c7899ddc5439789
Polina1441 <14042003dp@mail.ru>
2024-09-06 23:29:32 +0600

    aaa

--- src/pages/root.tsx
+++ src/pages/root.tsx
@@ -1,19 +1,32 @@
 import "./root.css";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const HeaderRoot = () => {
-    return (
-        <div className={"frameParent"}>
-            <div className={"rectangleParent"}>
-                <div className={"frameChild"} />
-                <div className={"ai"}>ai</div>
-                <div className={"p"}>P</div>
-            </div>
-            <div className={"frameItem"} />
-        </div>);
+    return (<Container>
+        <Row className="justify-content-md-center">
+            <Col xs lg="2">
+                1 of 3
+            </Col>
+            <Col md="auto">Variable width content</Col>
+            <Col xs lg="2">
+                3 of 3
+            </Col>
+        </Row>
+        <Row>
+            <Col style={{background: "yellow"}}>1 of 3</Col>
+            <Col md="auto">Variable width content</Col>
+            <Col xs lg="2" style={{background: "blue"}}>
+                3 of 3
+            </Col>
+        </Row>
+    </Container>);
 }
 
 const Root = () => {
-    return <><HeaderRoot/><li>Error.....................</li></>
+    return <><HeaderRoot/>
+        <li>Error.....................</li></>
 }
 
 export default Root;
