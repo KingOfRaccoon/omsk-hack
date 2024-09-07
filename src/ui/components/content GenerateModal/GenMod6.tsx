23a0f8aee27e91ca20329bf26656dfb801ff5bad
Polina1441 <14042003dp@mail.ru>
2024-09-08 02:23:17 +0600

    step 6 in gen mod

--- src/ui/components/content GenerateModal/GenMod6.tsx
+++ src/ui/components/content GenerateModal/GenMod6.tsx
@@ -1,5 +1,6 @@
 import gen from "../../../assets/gen.svg";
 import {useEffect, useState} from "react";
+import {ToggleButtonGroup} from "react-bootstrap";
 
 interface GenModProps {
     nextPage: () => void,
@@ -9,6 +10,11 @@ interface GenModProps {
 
 export default function GenMod6(props: GenModProps) {
     const [percentage, setPercentage] = useState(0);
+    const [selectedButton, setSelectedButton] = useState(1);
+    const handleToggle = (value: number) => {
+        console.log(value);
+        setSelectedButton(value);
+    }
 
     useEffect(() => {
         if (props.isCurrentPage) {
@@ -32,6 +38,37 @@ export default function GenMod6(props: GenModProps) {
         </div>
 
         <h3 className="mt-4">Выбери оформление</h3>
+
+        <ToggleButtonGroup className="gap-2 mt-3 mb-2" type="radio" name="options" defaultValue={2}>
+            <button
+                className={selectedButton == 1 ? "align-items-center hstack m-0 p-3 br-36 align-items-center mx-auto bg-fio" : "align-items-center hstack m-0 p-3 bg-grey-3 br-36 align-items-center mx-auto"}
+                id="tbg-radio-1" value={1} onClick={() => handleToggle(1)}>
+                <div className="text-start">
+                    <h3 className={selectedButton == 1 ? "fc-white" : "fc-fio"}>3-5</h3>
+                </div>
+            </button>
+            <button
+                className={selectedButton == 2 ? "align-items-center hstack m-0 p-3 br-36 align-items-center mx-auto bg-fio" : "align-items-center hstack m-0 p-3 bg-grey-3 br-36 align-items-center mx-auto"}
+                id="tbg-radio-2" value={2} onClick={() => handleToggle(2)}>
+                <div className="text-start">
+                    <h3 className={selectedButton == 2 ? "fc-white" : "fc-fio"}>6-10</h3>
+                </div>
+            </button>
+            <button
+                className={selectedButton == 3 ? "align-items-center hstack m-0 p-3 br-36 align-items-center mx-auto bg-fio" : "align-items-center hstack m-0 p-3 bg-grey-3 br-36 align-items-center mx-auto"}
+                id="tbg-radio-4" value={3} onClick={() => handleToggle(3)}>
+                <div className="text-start">
+                    <h3 className={selectedButton == 3 ? "fc-white" : "fc-fio"}>11-15</h3>
+                </div>
+            </button>
+            <button
+                className={selectedButton == 4 ? "align-items-center hstack m-0 p-3 br-36 align-items-center mx-auto bg-fio" : "align-items-center hstack m-0 p-3 bg-grey-3 br-36 align-items-center mx-auto"}
+                id="tbg-radio-4" value={3} onClick={() => handleToggle(4)}>
+                <div className="text-start">
+                    <h3 className={selectedButton == 4 ? "fc-white" : "fc-fio"}>&rsaquo;15</h3>
+                </div>
+            </button>
+        </ToggleButtonGroup>
 
         <div className="row justify-content-between mt-4">
             <button className=" col-auto align-items-center hstack m-0 p-3 bg-f br-36 align-items-center">
