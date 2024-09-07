c3a45a0c431d336c7d30a7a569ea813fe685858c
Polina1441 <14042003dp@mail.ru>
2024-09-08 02:10:24 +0600

    step style start in gen mod

--- src/ui/components/content GenerateModal/GenMod4.tsx
+++ src/ui/components/content GenerateModal/GenMod4.tsx
@@ -3,11 +3,13 @@ import gen from "../../../assets/gen.svg";
 import {useEffect, useState} from "react";
 
 interface GenModProps {
-    nextPage: () => void;
-    isCurrentPage: boolean
+    nextPage: () => void,
+    isCurrentPage: boolean,
+    prevPage?: () => void
 }
 
-export default function GenMod4({props: GenModProps}) {
+export default function GenMod4(props: GenModProps) {
+    const [percentage, setPercentage] = useState(0);
     const [selectedButton, setSelectedButton] = useState(1);
     const handleToggle = (value: number) => {
         console.log(value);
@@ -22,7 +24,7 @@ export default function GenMod4({props: GenModProps}) {
             } else {
                 setTimeout(() => {
                     const newValue = percentage + Math.floor(Math.random() * 14) + 1
-                    setPercentage(newValue > 100 ? 100: newValue)
+                    setPercentage(newValue > 100 ? 100 : newValue)
                 }, 300)
             }
         }
@@ -39,21 +41,21 @@ export default function GenMod4({props: GenModProps}) {
         <ToggleButtonGroup className="gap-2 mt-3 mb-2" type="radio" name="options" defaultValue={2}>
             <button
                 className={selectedButton == 1 ? "align-items-center hstack m-0 p-3 br-36 align-items-center mx-auto bg-fio" : "align-items-center hstack m-0 p-3 bg-grey-3 br-36 align-items-center mx-auto"}
-                    id="tbg-radio-1" value={1} onClick={() => handleToggle(1)}>
+                id="tbg-radio-1" value={1} onClick={() => handleToggle(1)}>
                 <div className="text-start">
                     <h3 className={selectedButton == 1 ? "fc-white" : "fc-fio"}>3-5</h3>
                 </div>
             </button>
             <button
                 className={selectedButton == 2 ? "align-items-center hstack m-0 p-3 br-36 align-items-center mx-auto bg-fio" : "align-items-center hstack m-0 p-3 bg-grey-3 br-36 align-items-center mx-auto"}
-                    id="tbg-radio-2" value={2} onClick={() => handleToggle(2)}>
+                id="tbg-radio-2" value={2} onClick={() => handleToggle(2)}>
                 <div className="text-start">
                     <h3 className={selectedButton == 2 ? "fc-white" : "fc-fio"}>6-10</h3>
                 </div>
             </button>
             <button
                 className={selectedButton == 3 ? "align-items-center hstack m-0 p-3 br-36 align-items-center mx-auto bg-fio" : "align-items-center hstack m-0 p-3 bg-grey-3 br-36 align-items-center mx-auto"}
-                    id="tbg-radio-4" value={3} onClick={() => handleToggle(3)}>
+                id="tbg-radio-4" value={3} onClick={() => handleToggle(3)}>
                 <div className="text-start">
                     <h3 className={selectedButton == 3 ? "fc-white" : "fc-fio"}>11-15</h3>
                 </div>
@@ -75,24 +77,15 @@ export default function GenMod4({props: GenModProps}) {
             <span className="input-group-text" id="basic-addon2">слайдов</span>
         </div>
 
-        <div className="row justify-content-between mt-4">
-            <button className=" col-auto align-items-center hstack m-0 p-3 bg-f br-36 align-items-center">
-                <div className="text-start">
-                    <h3 className="fc-fio">Назад</h3>
-                </div>
-            </button>
-            <button className="col-auto align-items-center hstack m-0 p-3 bg-fio br-36 align-items-center"
-                onClick={props.nextPage}
-            >
-                <div className="icon-box bg-white">
-                    <img src={gen} alt=""/>
-                </div>
-                <div className="text-start ms-2">
-                    <h3 className="fc-white">Начать генерацию</h3>
-                </div>
-            </button>
-            <div className="col-1"></div>
-        </div>
+        <button className="align-items-center hstack align-items-center hstack m-0 p-3 bg-fio br-36 align-items-center mx-auto"
+                onClick={props.nextPage}>
+            <div className="icon-box bg-white">
+                <img src={gen} alt=""/>
+            </div>
+            <div className="text-start ms-2">
+                <h3 className="fc-white">Начать генерацию</h3>
+            </div>
+        </button>
 
     </div>
 
