1219ec199e455e8ba2681a561170b3a2533ce090
KingOfRaccoon <zvezdakov2015@yandex.ru>
2024-09-08 00:04:17 +0600

    create login page

--- src/ui/controllers/GenerateModalController.tsx
+++ src/ui/controllers/GenerateModalController.tsx
@@ -1,19 +1,25 @@
-import {useRef} from "react";
+import {useRef, useState} from "react";
 import {Swiper, SwiperRef, SwiperSlide} from "swiper/react";
 import GenMod1 from "../components/content GenerateModal/GenMod1.tsx";
 import GenMod2 from "../components/content GenerateModal/GenMod2.tsx";
 import GenMod3 from "../components/content GenerateModal/GenMod3.tsx";
 import GenMod4 from "../components/content GenerateModal/GenMod4.tsx";
 
-export function GenerateModalController(){
+export function GenerateModalController() {
+    const [index, setIndex] = useState(0);
     const swipeRef = useRef<SwiperRef>(null);
 
-    function nextPage(){
+    function nextPage() {
         if (swipeRef.current)
             swipeRef.current.swiper.slideNext();
     }
 
-    return <Swiper ref={swipeRef} noSwiping={false} noSwipingClass={'swiper-slide'} allowTouchMove={false}>
+    return <Swiper
+        ref={swipeRef}
+        noSwiping={false}
+        noSwipingClass={'swiper-slide'}
+        allowTouchMove={false}
+        onActiveIndexChange={it => {setIndex(it.activeIndex); console.log(it.activeIndex)}}>
         <SwiperSlide>
             <GenMod1 nextPage={nextPage}/>
         </SwiperSlide>
@@ -21,7 +27,7 @@ export function GenerateModalController(){
             <GenMod2 nextPage={nextPage}/>
         </SwiperSlide>
         <SwiperSlide>
-            <GenMod3 nextPage={nextPage}/>
+            <GenMod3 isCurrentPage={index == 2} nextPage={nextPage}/>
         </SwiperSlide>
         <SwiperSlide>
             <GenMod4/>
