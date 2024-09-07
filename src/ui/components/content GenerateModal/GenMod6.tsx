030d5824e4b61b725690e4cce76fb0c7e36484bd
Polina1441 <14042003dp@mail.ru>
2024-09-08 03:15:19 +0600

    step 6 in gen mod

--- src/ui/components/content GenerateModal/GenMod6.tsx
+++ src/ui/components/content GenerateModal/GenMod6.tsx
@@ -1,4 +1,10 @@
 import gen from "../../../assets/gen.svg";
+import temp1 from "../../../assets/temp1.png";
+import temp2 from "../../../assets/temp2.png";
+import temp3 from "../../../assets/temp3.png";
+import temp4 from "../../../assets/temp4.png";
+import temp5 from "../../../assets/temp5.png";
+import temp6 from "../../../assets/temp6.png";
 import {useEffect, useState} from "react";
 import {ToggleButtonGroup} from "react-bootstrap";
 
@@ -39,55 +45,52 @@ export default function GenMod6(props: GenModProps) {
 
         <h3 className="mt-4">Выбери оформление</h3>
 
-        <ToggleButtonGroup className="gap-2 mt-3 mb-2" type="radio" name="options" defaultValue={2}>
+        <ToggleButtonGroup id="templates" className="gap-2 mt-3 mb-2 horizontal-scrollable" type="radio" name="options" defaultValue={2}>
             <button
-                className={selectedButton == 1 ? "align-items-center hstack m-0 p-3 br-36 align-items-center mx-auto bg-fio" : "align-items-center hstack m-0 p-3 bg-grey-3 br-36 align-items-center mx-auto"}
+                className={selectedButton == 1 ? "vstack m-0 p-1 br-12 bg-fio" : "vstack m-0 p-1 br-12 bg-grey-3"}
                 id="tbg-radio-1" value={1} onClick={() => handleToggle(1)}>
-                <div className="text-start">
-                    <h3 className={selectedButton == 1 ? "fc-white" : "fc-fio"}>3-5</h3>
-                </div>
+                    <img src={temp1} alt=""/>
+                    <h4 className={selectedButton == 1 ? "fc-white fc-fio mx-auto my-1" : "fc-fio fc-fio mx-auto my-1"}>Департамент №1</h4>
             </button>
             <button
-                className={selectedButton == 2 ? "align-items-center hstack m-0 p-3 br-36 align-items-center mx-auto bg-fio" : "align-items-center hstack m-0 p-3 bg-grey-3 br-36 align-items-center mx-auto"}
+                className={selectedButton == 2 ? "vstack m-0 p-1 br-12 bg-fio" : "vstack m-0 p-1 br-12 bg-grey-3"}
                 id="tbg-radio-2" value={2} onClick={() => handleToggle(2)}>
-                <div className="text-start">
-                    <h3 className={selectedButton == 2 ? "fc-white" : "fc-fio"}>6-10</h3>
-                </div>
+                    <img src={temp2} alt=""/>
+                    <h4 className={selectedButton == 2 ? "fc-white fc-fio mx-auto my-1" : "fc-fio fc-fio mx-auto my-1"}>Департамент №2</h4>
             </button>
             <button
-                className={selectedButton == 3 ? "align-items-center hstack m-0 p-3 br-36 align-items-center mx-auto bg-fio" : "align-items-center hstack m-0 p-3 bg-grey-3 br-36 align-items-center mx-auto"}
+                className={selectedButton == 3 ? "vstack m-0 p-1 br-12 bg-fio" : "vstack m-0 p-1 br-12 bg-grey-3"}
                 id="tbg-radio-4" value={3} onClick={() => handleToggle(3)}>
-                <div className="text-start">
-                    <h3 className={selectedButton == 3 ? "fc-white" : "fc-fio"}>11-15</h3>
-                </div>
+                    <img src={temp3} alt=""/>
+                    <h4 className={selectedButton == 3 ? "fc-white fc-fio mx-auto my-1" : "fc-fio fc-fio mx-auto my-1"}>Яркая</h4>
             </button>
             <button
-                className={selectedButton == 4 ? "align-items-center hstack m-0 p-3 br-36 align-items-center mx-auto bg-fio" : "align-items-center hstack m-0 p-3 bg-grey-3 br-36 align-items-center mx-auto"}
+                className={selectedButton == 4 ? "vstack m-0 p-1 br-12 bg-fio" : "vstack m-0 p-1 br-12 bg-grey-3"}
                 id="tbg-radio-4" value={3} onClick={() => handleToggle(4)}>
-                <div className="text-start">
-                    <h3 className={selectedButton == 4 ? "fc-white" : "fc-fio"}>&rsaquo;15</h3>
-                </div>
+                    <img src={temp4} alt=""/>
+                    <h4 className={selectedButton == 4 ? "fc-white fc-fio mx-auto my-1" : "fc-fiofc-fio mx-auto my-1"}>Круглая</h4>
             </button>
         </ToggleButtonGroup>
 
-        <div className="row justify-content-between mt-4">
-            <button className=" col-auto align-items-center hstack m-0 p-3 bg-f br-36 align-items-center">
-                <div className="text-start">
-                    <h3 className="fc-fio"  onClick={props.prevPage}>Назад</h3>
-                </div>
-            </button>
-            <button className="col-auto align-items-center hstack m-0 p-3 bg-fio br-36 align-items-center"
-                    onClick={props.nextPage}>
-                <div className="icon-box bg-white">
-                    <img src={gen} alt=""/>
-                </div>
-                <div className="text-start ms-2">
-                    <h3 className="fc-white">Начать генерацию</h3>
-                </div>
-            </button>
-            <div className="col-1"></div>
+        <div className="container">
+            <div className="row justify-content-between mt-4">
+                <button className=" col-auto align-items-center hstack m-0 p-3 bg-f br-36 align-items-center">
+                    <div className="text-start">
+                        <h3 className="fc-fio" onClick={props.prevPage}>Назад</h3>
+                    </div>
+                </button>
+                <button className="col-auto align-items-center hstack m-0 p-3 bg-fio br-36 align-items-center"
+                        onClick={props.nextPage}>
+                    <div className="icon-box bg-white">
+                        <img src={gen} alt=""/>
+                    </div>
+                    <div className="text-start ms-2">
+                        <h3 className="fc-white">Начать генерацию</h3>
+                    </div>
+                </button>
+                <div className="col-1"></div>
+            </div>
         </div>
-
     </div>
 
 }
