df457b722d2c35daf9d2790854acc95eafff52a4
Polina1441 <14042003dp@mail.ru>
2024-09-07 19:01:05 +0600

    step 4 in generate modal

---
+++ src/ui/components/content GenerateModal/GenMod4.tsx
@@ -0,0 +1,14 @@
+export default function GenMod4({}) {
+    return <div>
+
+        {/*из файла -4- */}
+        <div className="row">
+            <h2>Генерация презентации</h2>
+            <small className="fc-grey-50">шаг 2 из 3</small>
+        </div>
+
+        <h3 className="mt-3">Сколько слайдов хочешь получить?</h3>
+
+    </div>
+
+}
