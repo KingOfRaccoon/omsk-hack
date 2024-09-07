4c328e4b5dc1ff25ae573c309f4bdb4d0e4411d0
Polina1441 <14042003dp@mail.ru>
2024-09-08 01:43:13 +0600

    fix step 4 gen modal

--- src/ui/components/content GenerateModal/GenMod4.tsx
+++ src/ui/components/content GenerateModal/GenMod4.tsx
@@ -11,7 +11,7 @@ export default function GenMod4({}) {
 
         <h3 className="mt-4">Сколько слайдов хочешь получить?</h3>
 
-        <ToggleButtonGroup className="gap-2 mt-2 mb-2" type="radio" name="options" defaultValue={2}>
+        <ToggleButtonGroup className="gap-2 mt-3 mb-2" type="radio" name="options" defaultValue={2}>
             <button className="align-items-center hstack m-0 p-3 bg-grey-3 br-36 align-items-center mx-auto"
                     id="tbg-radio-1" value={1}>
                 <div className="text-start">
@@ -33,30 +33,38 @@ export default function GenMod4({}) {
             <button className="align-items-center hstack m-0 p-3 bg-grey-3 br-36 align-items-center mx-auto"
                     id="tbg-radio-3" value={3}>
                 <div className="text-start">
-                    <h3 className="fc-fio">&lsaquo;15</h3>
+                    <h3 className="fc-fio">&rsaquo;15</h3>
                 </div>
             </button>
         </ToggleButtonGroup>
 
-        <small className="fc-grey-50 d-block my-3">или введи точное количество</small>
+        <small className="fc-grey-50 d-block my-4">или введи точное количество</small>
 
-
-        <div className="input-group mb-3">
+        <div className="input-group mb-3 justify-content-center">
             <input type="text" className="form-control text-field shadow-none no-border" placeholder="10"
-                   aria-label="Recipient's username" aria-describedby="basic-addon2"/>
+                   aria-label="Number of slides" aria-describedby="basic-addon2"/>
             <span className="input-group-text" id="basic-addon2">слайдов</span>
         </div>
 
-        <button className="align-items-center hstack m-0 p-3 bg-fio br-36 align-items-center mx-auto mt-4 mb-2"
+        <div className="row justify-content-between mt-4">
+            <button className=" col-auto align-items-center hstack m-0 p-3 bg-f br-36 align-items-center">
+                <div className="text-start">
+                    <h3 className="fc-fio">Назад</h3>
+                </div>
+            </button>
+            <button className="col-auto align-items-center hstack m-0 p-3 bg-fio br-36 align-items-center"
                 // onClick={props.nextPage}
-        >
-            <div className="icon-box bg-white">
-                <img src={gen} alt=""/>
-            </div>
-            <div className="text-start ms-2">
-                <h3 className="fc-white">Начать генерацию</h3>
-            </div>
-        </button>
+            >
+                <div className="icon-box bg-white">
+                    <img src={gen} alt=""/>
+                </div>
+                <div className="text-start ms-2">
+                    <h3 className="fc-white">Начать генерацию</h3>
+                </div>
+            </button>
+            <div className="col-1"></div>
+        </div>
+
     </div>
 
 }
